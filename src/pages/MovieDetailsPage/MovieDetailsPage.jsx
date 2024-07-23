830d5416d0777d6c16ba5c60c1b8a352de3a60cd
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-07-23 10:40:04 +0300

    MovieDetailsPage

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import { fetchMovieDetails } from "../../movies-api";
 import { useParams } from "react-router-dom";
+import css from "./MovieDetailsPage.module.css";
 
 const MovieDetailsPage = () => {
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState(null);
   //   const [loading, setLoading] = useState(false);
   //   const [error, setError] = useState(false);
   const params = useParams();
+  const imgUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
   useEffect(() => {
     async function fetchMovie() {
@@ -32,18 +34,24 @@ const MovieDetailsPage = () => {
 
       {movie && (
         <div>
-          <h1>
-            {movie.original_title} ({movie.release_date.slice(0, 4)})
-          </h1>
-          <p>User score: {Math.round(movie.vote_average * 10)}%</p>
-          <h2>Overview</h2>
-          <p>{movie.overview}</p>
-          <h2>Genres</h2>
-          {/* <ul>
+          <div className={css.container}>
+            <img src={imgUrl} alt="photo" width="320" />
+
+            <div>
+              <h1>
+                {movie.original_title} ({movie.release_date})
+              </h1>
+              <p>User score: {Math.round(movie.vote_average * 10)}%</p>
+              <h2>Overview</h2>
+              <p>{movie.overview}</p>
+              <h2>Genres</h2>
+              {/* <ul>
             {movie.genres.map(({ id, name }) => (
               <li key={id}>{name}</li>
             ))}
           </ul> */}
+            </div>
+          </div>
         </div>
       )}
     </div>
