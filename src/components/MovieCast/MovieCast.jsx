b3cc665e5dbc893aba47df36fa6511e10b460942
olhaHrb <150930419+olhaHrb@users.noreply.github.com>
2024-07-30 16:47:27 +0300

    movie cast ok

--- src/components/MovieCast/MovieCast.jsx
+++ src/components/MovieCast/MovieCast.jsx
@@ -1,9 +1,45 @@
-const MovieCast = ({ data }) => {
-  console.log({ data });
+import { useEffect, useState } from "react";
+import { fetchMovieCast } from "../../movies-api";
+import { useParams } from "react-router-dom";
+import css from "./MovieCast.module.css";
+
+const MovieCast = () => {
+  const [cast, setCast] = useState(null);
+  const params = useParams();
+  useEffect(() => {
+    async function fetchCast() {
+      try {
+        const data = await fetchMovieCast(params.movieId);
+        setCast(data.cast);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchCast();
+  }, [params.movieId]);
+
+  console.log(cast);
+
   return (
-    <>
-      <p>MovieCast</p>
-    </>
+    <div>
+      {cast !== null && (
+        <ul className={css.list}>
+          {cast.map(({ id, name, profile_path }) => (
+            <li key={id}>
+              <div className={css.item}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w500/${profile_path}`}
+                  alt="photo"
+                  width="120"
+                />
+
+                {name}
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
   );
 };
 
--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { fetchMovieDetails, fetchMovieCast } from "../../movies-api";
+import { fetchMovieDetails } from "../../movies-api";
 import Navigation from "../../components/Navigation/Navigation";
 import { useParams, NavLink, Outlet } from "react-router-dom";
 import css from "./MovieDetailsPage.module.css";
@@ -7,7 +7,7 @@ import { FiArrowLeft } from "react-icons/fi";
 
 const MovieDetailsPage = () => {
   const [movie, setMovie] = useState(null);
-  const [cast, setCast] = useState(null);
+  // const [cast, setCast] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const params = useParams();
@@ -27,20 +27,20 @@ const MovieDetailsPage = () => {
     fetchMovie();
   }, [params.movieId]);
 
-  useEffect(() => {
-    async function fetchCast() {
-      try {
-        const data = await fetchMovieCast(params.movieId);
-        setCast(data.cast);
-        // console.log(data.cast);
-      } catch (error) {
-        setError(true);
-      }
-    }
-    fetchCast();
-  }, [params.movieId]);
+  // useEffect(() => {
+  //   async function fetchCast() {
+  //     try {
+  //       const data = await fetchMovieCast(params.movieId);
+  //       setCast(data.cast);
+  //       // console.log(data.cast);
+  //     } catch (error) {
+  //       setError(true);
+  //     }
+  //   }
+  //   fetchCast();
+  // }, [params.movieId]);
 
-  console.log(cast);
+  // console.log(cast);
 
   return (
     <div>
@@ -86,9 +86,7 @@ const MovieDetailsPage = () => {
             <p>Additional information</p>
             <ul>
               <li>
-                <NavLink to="cast" data={cast}>
-                  Cast
-                </NavLink>
+                <NavLink to="cast">Cast</NavLink>
               </li>
               <li>
                 <NavLink to="reviews">Reviews</NavLink>
